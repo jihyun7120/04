520e9d9aff032faeb616c1a5ea119efc8291e406
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-09-25 16:13:33 +0900

    practice5

--- main.c
+++ main.c
@@ -2,22 +2,24 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
-{
-    int year;
-    int x;
-     
-    printf("input year: ");
-    scanf("%i", &year);
+{  
+   unsigned int x;
+   int b;
+   
+   printf("input a number: ");
+   scanf("%ui", &x);
+   
+   for (b=0; x!=0; x>>=1)
+   {
+       if(x&1)
+       
+              b++;
+              
+   }
+              
+   printf("The result is : %i\n", b);
+   
     
-    
-    x = (year%4==0 && year%100!=0) || (year % 400 ==0);
-    
-    printf("is the year %i the leap year? : %i\n", year, x);
-    
-    
-  //윤년: 4로 나누어 떨어지면서 100으로 나누어떨어지지않으면 
-  //또는 400으로 나누어 떨어지면 윤년 
-  
   system("PAUSE");	
   return 0;
 }
