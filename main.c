9cd727106732561770cb6afd350e1a1de032749f
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-09-25 16:20:53 +0900

    practice6

--- main.c
+++ main.c
@@ -3,22 +3,18 @@
 
 int main(int argc, char *argv[])
 {  
-   unsigned int x;
-   int b;
-   
-   printf("input a number: ");
-   scanf("%ui", &x);
-   
-   for (b=0; x!=0; x>>=1)
-   {
-       if(x&1)
-       
-              b++;
-              
-   }
-              
-   printf("The result is : %i\n", b);
-   
+    int x;
+    int hour, min, sec;
+    
+    printf("input the second: ");
+    scanf("%i", &x); 
+    
+    hour = x / 3600;
+    min = (x%3600) / 60;
+    sec = x % 60;
+    
+    printf("The time for %i second is %i:%i:%i", x, hour, min, sec);
+  
     
   system("PAUSE");	
   return 0;
