80e1bb87b49e841de4494ac50218ca0d6abacc5d
jihyun7120 <jihyun7120@sookmyung.ac.kr>
2025-09-25 15:57:55 +0900

    practice4

--- main.c
+++ main.c
@@ -3,16 +3,20 @@
 
 int main(int argc, char *argv[])
 {
-    int x, m, s;
-    printf("input the second: ");
-    scanf("%i", &x);
-    
-    m = x/60;
-    s = x%60;
-    
-    printf("The time is %i:%i", m, s);
+    int year;
+    int x;
+     
+    printf("input year: ");
+    scanf("%i", &year);
     
     
+    x = (year%4==0 && year%100!=0) || (year % 400 ==0);
+    
+    printf("is the year %i the leap year? : %i\n", year, x);
+    
+    
+  //윤년: 4로 나누어 떨어지면서 100으로 나누어떨어지지않으면 
+  //또는 400으로 나누어 떨어지면 윤년 
   
   system("PAUSE");	
   return 0;
